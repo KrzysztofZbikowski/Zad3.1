3845ff218963d337819fd0d62bcf2bbc52623b9d
Zbiku <Zbiku@MSI>
2023-10-26 22:41:47 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int rok;
+        Console.WriteLine("Podaj rok: ");
+        rok = int.Parse(Console.ReadLine());
+        if(rok%4==0)
+        {
+            if(rok%100!=0)
+            {
+                Console.WriteLine("Jest to rok przestępny");
+            }
+            else if (rok%400==0)
+            {
+                Console.WriteLine("Jest to rok przestępny");
+            }
+            else
+            {
+                Console.WriteLine("Nie jest to rok przestępny");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nie jest to rok przestępny");
+        }
+        Console.ReadKey();
+    }
+}
